7ea4059e2be32a49d9e0830611157515fec83e55
gancsdavid0 <gancsdavid0@gmail.com>
2023-11-20 14:06:53 +0100

    A képzésekhez tartozó útvonalak meghatározása

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 require('dotenv').config() //.env fájlt olvassa
 
+const tranings = require('./routes/tranings')
+
+
 const app = express()
+
+app.use('/api/trainings', tranings)
+
 
 app.listen(process.env.PORT, ()=>
     console.log(`Server is running on port: ${process.env.PORT}`
